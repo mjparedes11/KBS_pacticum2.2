090ae3b816106183d4da3dd1bcb751742ddfd8e7
unknown <mjparedes11@utpl.edu.ec>
2026-06-03 23:56:40 -0500

    migraciones

--- mongo-init/import.sh
+++ mongo-init/import.sh
@@ -1,12 +1,39 @@
 #!/bin/bash
 
-echo "Importando students.json..."
-
+echo "Importando jsons..."
+#active_versions
 mongoimport \
   --username admin \
   --password admin123 \
   --authenticationDatabase admin \
-  --db school \
-  --collection students \
-  --file /data/import/students.json \
+  --db edx \
+  --collection active_versions \
+  --file /data/import/modulestore.active_versions.json
+
+#definitions
+mongoimport \
+  --username admin \
+  --password admin123 \
+  --authenticationDatabase admin \
+  --db edx \
+  --collection definitions \
+  --file /data/import/modulestore.definitions.json
+
+#structures
+mongoimport \
+  --username admin \
+  --password admin123 \
+  --authenticationDatabase admin \
+  --db edx \
+  --collection structures \
+  --file /data/import/modulestore.structures.json
+
+#structure02
+mongoimport \
+  --username admin \
+  --password admin123 \
+  --authenticationDatabase admin \
+  --db edx \
+  --collection structure02 \
+  --file /data/import/modulestore.structure02es.json \
   --jsonArray
